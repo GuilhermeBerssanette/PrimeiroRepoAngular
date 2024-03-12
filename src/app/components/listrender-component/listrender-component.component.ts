ca680a7ed13f9f7d3ef3777b81d1c626b37efcd1
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-03-12 01:55:14 -0300

    Utilizando interfaces

--- src/app/components/listrender-component/listrender-component.component.ts
+++ src/app/components/listrender-component/listrender-component.component.ts
@@ -1,6 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import { Animal } from '../../Animal';
 @Component({
   selector: 'app-listrender-component',
   standalone: true,
@@ -10,14 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListrenderComponentComponent implements OnInit{
 
-animals = [
-  { name: 'turca', type: 'dog'},
-  { name: 'tom', type: 'cat'},
-  { name: 'frida', type: 'dog'},
-  { name: 'bob', type: 'horse'},
-]
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 4 },
+    { name: 'Tom', type: 'Cat', age: 10 },
+    { name: 'Frida', type: 'Dog', age: 11 },
+    { name: 'Bob', type: 'Horse', age: 2 },
+  ];
 
-constructor() {}
+  animalDetails = '';
+
+  constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal): void{
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos de vida!`;
+  }
 }
